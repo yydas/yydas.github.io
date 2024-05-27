143fb063f4f356c8816ef2f2e7dddc4debcb7953
yydas <1208774030@qq.com>
2024-05-27 22:06:51 +0800

    配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
+  site: 'https://yydas.github.io',
   server:{
     host: '192.168.31.17',
   }
